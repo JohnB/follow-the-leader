ab437eb6710b5cfc03bcb2c85456863c8027943c
John Baylor <john@razoo.com>
2014-02-17 02:41:44 -0800

    Improved instructions

--- client/views/lead/lead.js
+++ client/views/lead/lead.js
@@ -2,5 +2,15 @@ Template['lead'].helpers({
 });
 
 Template['lead'].events({
+    'click .getlocation': function() {
+        if (navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(showPosition);
+            // TODO: figure out how to display the lat/lon
+        }
+        else
+        {
+            x.innerHTML="Geolocation is not supported by this browser.";
+        }
+    }
 });
-
