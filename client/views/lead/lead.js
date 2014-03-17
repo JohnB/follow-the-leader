766ef8a3dba24c6f379f1bfd2767c65e2cfed9c9
John Baylor <john@razoo.com>
2014-03-16 18:51:05 -0700

    Connected to google maps!

--- client/views/lead/lead.js
+++ client/views/lead/lead.js
@@ -28,8 +28,15 @@ Template.lead.events({
     'click .getlocation': function() {
         if (navigator.geolocation)
         {
+            var center = new google.maps.LatLng(51.508742,-0.120850);
+            var map = new google.maps.Map(document.getElementById("googleMap"),{
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
             navigator.geolocation.watchPosition(function (pos) {
                 Session.set('locationary', [pos.coords.latitude, pos.coords.longitude] );
+                center = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                map.setCenter(center);
             });
         }
         else
